9406c3be052459ab43a2e3232cbe12efa0d796db
Dex Plushy <zac.clennell@gmail.com>
2025-11-02 16:09:55 +0000

    Imgui, hash-library, nlohmann. Also moving git clone commands into a separate script

---
+++ Eidolist2/FetchGitLibraries.ps1
@@ -0,0 +1,3 @@
+git clone https://github.com/stbrumme/hash-library.git lib/hash-library
+git clone https://github.com/nlohmann/json.git lib/nlohmann
+git clone https://github.com/ocornut/imgui.git lib/imgui
