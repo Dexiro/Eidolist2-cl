57249bf1a6426ab0d2c90dc840ad44a0cd1b13e9
Dex Plushy <zac.clennell@gmail.com>
2025-11-04 01:49:56 +0000

    Update Project.cpp

--- Eidolist2/src/projectdata/Project.cpp
+++ Eidolist2/src/projectdata/Project.cpp
@@ -3,10 +3,7 @@
 
 Project::Project()
 {
-	for (int i = 0; i < m_data.size(); i++)
-	{
-		m_data[i] = std::make_unique<ProjectData>();
-	}
+	m_data = std::make_unique<ProjectData>();
 }
 
 Project::~Project()
@@ -14,32 +11,28 @@ Project::~Project()
 	Reset();
 }
 
-bool Project::LoadProject(int project, std::string directory)
+bool Project::LoadProject(std::string directory)
 {
 	bool success = true;
-	if(!LoadProject::LoadProject(m_data[project].get(), directory))
+	if(!LoadProject::LoadProject(m_data.get(), directory))
 	{
 		success = false;
 		Reset();
-	}
-	else
-	{
-		m_nLoadedProjects++;
 	}
 
 	return success;
 }
 
-MapData* Project::GetMapData(int project, int id, bool fullData)
+MapData* Project::GetMapData(int id, bool fullData)
 {
 	MapData* map_data = nullptr;
 
-	if (id < m_data[project]->m_map_data.size())
-		map_data = m_data[project]->m_map_data[id].get();
+	if (id < m_data->m_map_data.size())
+		map_data = m_data->m_map_data[id].get();
 
 	if (fullData && map_data && !map_data->lcfMap)
 	{
-		if (!LoadProject::LoadFullMapData(m_data[project].get(), map_data))
+		if (!LoadProject::LoadFullMapData(m_data.get(), map_data))
 		{
 			map_data = nullptr;
 		}
@@ -48,14 +41,9 @@ MapData* Project::GetMapData(int project, int id, bool fullData)
 	return map_data;
 }
 
-int Project::NMaps(int project)
+int Project::NMaps()
 {
-	return (int)m_data[project]->m_map_data.size();
-}
-
-int Project::NMapsLoaded(int project)
-{
-	return m_loadedMapsByProject[project].size();
+	return (int)m_data->m_map_data.size();
 }
 
 int Project::NMapsLoaded()
@@ -63,36 +51,35 @@ int Project::NMapsLoaded()
 	return m_loadedMaps.size();
 }
 
-ProjectData* Project::Data(int project)
+ProjectData* Project::Data()
 { 
-	return m_data[project].get();
+	return m_data.get();
 }
 
-std::string Project::Directory(int project)
+std::string Project::Directory()
 { 
-	return m_data[project]->m_directory;
+	return m_data->m_directory;
 }
 
-bool Project::LoadMapObj(SDLContext& context, int project, int mapId, glm::vec2& pos)
+bool Project::LoadMapObj(SDLContext& context, int mapId, glm::vec2& pos)
 {
-	for (auto mapObj : m_loadedMapsByProject[project])
+	for (auto mapObj : m_loadedMaps)
 	{
 		if (mapObj->m_mapData->map_id == mapId)
 			return true;
 	}
 
-	MapData* map_data = GetMapData(project, mapId, true);
+	MapData* map_data = GetMapData(mapId, true);
 
 	if (map_data != nullptr)
 	{
 		std::shared_ptr<MapObj> mapObj = std::make_shared<MapObj>();
 		mapObj->m_spritesheet = m_debugSpritesheet;
 		mapObj->LoadMap(context, map_data);
-		mapObj->CompileMap(context, m_data[project].get());
+		mapObj->CompileMap(context, m_data.get());
 		mapObj->m_pos = pos + m_mapCaret;
 		m_mapCaret.x += (mapObj->m_mapData->lcfMap->width * 16) + 128;
 		m_loadedMaps.push_back(mapObj);
-		m_loadedMapsByProject[project].push_back(mapObj);
 		return true;
 	}
 	return false;
@@ -104,33 +91,11 @@ bool Project::LoadRequestedMaps(SDLContext& context, glm::vec2& pos)
 
 	for (auto& i : m_requestLoadMapIDs)
 	{
-		LoadMapObj(context, m_selectedProject, i, pos);
+		LoadMapObj(context, i, pos);
 	}
 	m_requestLoadMapIDs.clear();
 
 	return true;
-}
-
-MapObj* Project::GetMapObjByIndex(int project, int index)
-{
-	MapObj* obj = nullptr;
-	if (m_loadedMapsByProject[project].size() > index)
-		obj = m_loadedMapsByProject[project][index].get();
-
-	return obj;
-}
-
-MapObj* Project::GetMapObjByID(int project, int mapId)
-{
-	MapObj* obj = nullptr;
-
-	for (auto& mapObj : m_loadedMapsByProject[project])
-	{
-		if (mapObj->m_mapData->map_id == mapId)
-			obj = mapObj.get();
-	}
-
-	return obj;
 }
 
 MapObj* Project::GetMapObjByID(int id)
@@ -164,25 +129,13 @@ std::vector<MapObj*> Project::GetCollisions(MapObj* target, glm::vec2 wpos, glm:
 
 void Project::Reset()
 {
-	for (int i = 0; i < m_data.size(); i++)
-	{
-		if (m_data[i]->m_lcf_db)
-			m_data[i]->m_lcf_db.reset();
+	m_data->m_lcf_db.reset();
+	m_data->m_lcf_mt.reset();
 
-		if (m_data[i]->m_lcf_mt)
-			m_data[i]->m_lcf_mt.reset();
+	for (auto& map_data : m_data->m_map_data)
+		map_data.reset();
 
-		for (auto& map_data : m_data[i]->m_map_data)
-			map_data.reset();
-	}
-
-	for (int i = 0; i < m_loadedMaps.size(); i++)
-	{
-		for (auto& map_obj : m_loadedMapsByProject[i])
-			map_obj.reset();
-
-		for (auto& map_obj : m_loadedMaps)
-			map_obj.reset();
-	}
+	for (auto& map_obj : m_loadedMaps)
+		map_obj.reset();
 }
 
