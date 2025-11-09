3787f1d4cf627b00b56f71e4ba66c09a94ab26d5
Dex Plushy <zac.clennell@gmail.com>
2025-11-09 12:47:50 +0000

    Delete FetchGitLibraries.ps1

--- Eidolist2/FetchGitLibraries.ps1
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/stbrumme/hash-library.git lib/hash-library
-git clone https://github.com/nlohmann/json.git lib/nlohmann
-git clone https://github.com/ocornut/imgui.git lib/imgui
